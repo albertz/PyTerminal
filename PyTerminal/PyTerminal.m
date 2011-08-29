9f609e2e27e46f4c1391afe13efa19310764a075
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-08-29 12:41:17 +0200

    initial PyTerminal

---
+++ PyTerminal/PyTerminal.m
@@ -0,0 +1,23 @@
+//
+//  PyTerminal.m
+//  PyTerminal
+//
+//  Created by Albert Zeyer on 29.08.11.
+//  Copyright 2011 Albert Zeyer. All rights reserved.
+//
+
+#import "PyTerminal.h"
+
+@implementation PyTerminal
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
